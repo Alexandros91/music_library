44037b9782363874e682392646f57d6911dbf528
AdminAlex <papagiannis.a.999@gmail.com>
2024-02-12 20:52:54 +0200

    Writing the design recipe file

---
+++ spec/seeds_artists.sql
@@ -0,0 +1,5 @@
+TRUNCATE TABLE artists RESTART IDENTITY; 
+
+
+INSERT INTO artists (name, genre) VALUES ('Anna Vissi', 'Laiko');
+INSERT INTO artists (name, genre) VALUES ('Natassa Mpofiliou', 'Entexno');
