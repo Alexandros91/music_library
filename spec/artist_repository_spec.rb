4cb92cd86e721fddd48041a33df023e2312a23c7
AdminAlex <papagiannis.a.999@gmail.com>
2024-02-12 21:10:02 +0200

    Test driving the all method for artist_repository class

---
+++ spec/artist_repository_spec.rb
@@ -0,0 +1,29 @@
+require 'artist_repository'
+
+RSpec.describe ArtistRepository do
+
+  def reset_artists_table
+    seed_sql = File.read('spec/seeds_artists.sql')
+    connection = PG.connect({ host: '127.0.0.1', dbname: 'music_library_test' })
+    connection.exec(seed_sql)
+  end
+
+  before(:each) do 
+    reset_artists_table
+  end
+
+  it 'returns the list of artists' do
+    repo = ArtistRepository.new
+    
+    artists = repo.all
+    expect(artists.length).to eq 2
+    
+    expect(artists[0].id).to eq 1
+    expect(artists[0].name).to eq 'Anna Vissi'
+    expect(artists[0].genre).to eq 'Laiko'
+    
+    expect(artists[1].id).to eq 2
+    expect(artists[1].name).to eq 'Natassa Mpofiliou'
+    expect(artists[1].genre).to eq 'Entexno'
+  end
+end
